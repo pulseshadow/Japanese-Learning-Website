562a06e3202cf9d2e5712b15a36998c67cf001c3
Jack <Jackcbutler00@gmail.com>
2025-09-04 18:44:05 -0400

    X

--- script.js
+++ script.js
@@ -7011,8 +7011,7 @@ function addCustomRound() {
         </div>
         
         <div class="custom-round-content" id="round-content-${roundNumber}">
-        <p data-en="Please select the words you'd like to include in this round." data-es="Por favor selecciona las palabras que quieres incluir en esta ronda." data-fr="Veuillez sélectionner les mots que vous souhaitez inclure dans cette ronde." data-ja="このラウンドに含めたい単語を選択してください。">Please select the words you'd like to include in this round.</p>
-        
+        <p data-en="Please select the words you'd like to include in this round." data-es="Por favor selecciona las palabras que quieres incluir en esta ronda." data-fr="Veuillez sélectionner les mots que vous souhaitez inclure dans cette ronde." data-ja="このラウンドに含めたい単語を選択してください。">Please select the words you'd like to include in this round.        </p>
         <div class="word-selection-grid" id="word-selection-${roundNumber}">
             <!-- Word checkboxes will be populated by JavaScript -->
         </div>
@@ -7897,9 +7896,8 @@ function loadCustomRounds() {
                 </div>
                 
                 <div class="custom-round-content" id="round-content-${roundNumber}">
-                <p data-en="Please select the words you'd like to include in this round." data-es="Por favor selecciona las palabras que quieres incluir en esta ronda." data-fr="Veuillez sélectionner les mots que vous souhaitez inclure dans cette ronde." data-ja="このラウンドに含めたい単語を選択してください。">Please select the words you'd like to include in this round.</p>
-                
-                <div class="word-selection-grid" id="word-selection-${roundNumber}">
+                <p data-en="Please select the words you'd like to include in this round." data-es="Por favor selecciona las palabras que quieres incluir en esta ronda." data-fr="Veuillez sélectionner les mots que vous souhaitez inclure dans cette ronde." data-ja="このラウンドに含めたい単語を選択してください。">Please select the words you'd like to include in this round.        </p>
+        <div class="word-selection-grid" id="word-selection-${roundNumber}">
                     <!-- Word checkboxes will be populated by JavaScript -->
                 </div>
                 
